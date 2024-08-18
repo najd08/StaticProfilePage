36eca19414f4f1f6d7e5d512208fe42da84ab28c
najd08 <120324320+najd08@users.noreply.github.com>
2024-08-18 19:11:01 +0300

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Najd Alsabi on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
