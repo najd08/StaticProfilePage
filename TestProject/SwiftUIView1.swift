7237585cc48571c71d0ca3b93c88cfad305d03ce
najd08 <120324320+najd08@users.noreply.github.com>
2024-08-21 22:48:10 +0300

    Project

---
+++ TestProject/SwiftUIView1.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView1.swift
+//  TestProject
+//
+//  Created by Najd Alsabi on 21/08/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView1()
+}
