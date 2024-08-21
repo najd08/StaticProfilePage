7eecd18ef1611a8df10a8dfb905fb040f5e223f6
najd08 <120324320+najd08@users.noreply.github.com>
2024-08-21 22:49:19 +0300

    proj

--- TestProject/SwiftUIView1.swift
+++ TestProject/SwiftUIView1.swift
@@ -7,12 +7,64 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct SwiftUIView1: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Profile")
+                .font(.title)
+                .fontWeight(.medium)
+                .padding(.bottom)
+            
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Rectangle()
+                        .fill(.clear)
+                        .border(Color.black)
+                        .frame(width: 100, height: 100)
+                    
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Name: Sara Ali").font(.title2).fontWeight(.medium)
+                        Text("Email:").font(.title2).fontWeight(.medium)
+                        Text("example@mail.com").font(.title2)
+                    }
+                }
+                .padding(.bottom)
+                
+                Text("Bio")
+                    .font(.title3)
+                
+                Rectangle()
+                    .fill(.clear)
+                    .border(Color.black)
+                    .frame(width: 300, height: 100)
+                
+                Text("Education")
+                    .font(.title3)
+                
+                Rectangle()
+                    .fill(.clear)
+                    .border(Color.black)
+                    .frame(width: 300, height: 100)
+                
+                Text("Skills")
+                    .font(.title3)
+                
+                Rectangle()
+                    .fill(.clear)
+                    .border(Color.black)
+                    .frame(width: 300, height: 100)
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
     SwiftUIView1()
 }
+
+// .trailing = right, .leading = left
